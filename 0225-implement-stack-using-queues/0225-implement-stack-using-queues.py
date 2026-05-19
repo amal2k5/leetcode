ee3ec1fc0a2ddfe373b168a0a3c5a5a418b2bdf7
Amal Krishna A <amalkrishnapocox2@gmail.com>
2026-05-19 12:20:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (84.25%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -7,9 +7,11 @@ class MyStack:
 
     def push(self, x: int) -> None:
         self.q2.append(x)
+
         while self.q1:
             self.q2.append(self.q1.popleft())
         self.q1, self.q2 = self.q2, self.q1
+        
 
     def pop(self) -> int:
         return self.q1.popleft()
