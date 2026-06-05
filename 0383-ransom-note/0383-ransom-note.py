ba1208866bd28758750dd060a765eb658ebce353
Amal Krishna A <amalkrishnapocox2@gmail.com>
2026-06-05 14:19:38 +0530

    Time: 23 ms (53.69%), Space: 19.5 MB (72.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,12 +3,13 @@ class Solution:
 
         mag_count = {}
 
-        for ch in magazine:
-            mag_count[ch] = mag_count.get(ch, 0) + 1
+        for ch1 in magazine:
+            mag_count[ch1] = mag_count.get(ch1, 0) + 1
 
         for ch2 in ransomNote:
             if mag_count.get(ch2, 0) == 0:
                 return False
             mag_count[ch2] -= 1 
+
         return True            
         
