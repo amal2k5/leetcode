d454d2350ac3b24c63fcdd24cdcd105dccc387c3
Amal Krishna A <amalkrishnapocox2@gmail.com>
2026-06-25 11:39:44 +0530

    Time: 0 ms (100%), Space: 12.3 MB (58.56%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.py
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -4,10 +4,10 @@ class Solution(object):
         result = set()
         morse =  {'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".", 'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---", 'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---", 'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-", 'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--.."}
 
-        for w in words:
+        for word in words:
             morseCode = ''
-            
-            for m in w:
+
+            for m in word:
                 morseCode += morse[m]
             result.add(morseCode)
 
